e7b7a606cdd8a5d26a38460ae16e261bac4652ad
AlexBuenrostro <alex101211b@gmail.com>
2019-10-18 17:11:36 -0500

    Card component created, waiting to create correct rotation to the images

---
+++ src/components/CardItems/Description.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Description = (props) => (
+    <h4>{props.description}</h4>
+);
+
+export default Description;
---
+++ src/components/CardItems/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CardItems.css';
+
+const Image = (props) => (
+    <img 
+        className="Image"
+        alt={props.alt}
+        src={props.image} />
+);
+
+export default Image;
